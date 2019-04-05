c361d712c164e61d59d07615e0f68898aad7039a
Susan Almassi <susan.z.almassi@gmail.com>
2019-04-05 11:37:18 +0200

    everything's working except new game button

--- game.js
+++ game.js
@@ -3,6 +3,12 @@ var winningScore = (document.querySelector(".winningScore").textContent = "");
 var score = [0, 0];
 var currentValue = 0;
 var activePlayer = 0;
+var die1Random;
+var die2Random;
+function random() {
+  var dieRandom = Math.floor(Math.random() * 6 + 1);
+  return dieRandom;
+}
 
 function init() {
   const winningScoreInputReset = (document.querySelector(
@@ -45,8 +51,8 @@ document.getElementById("btnNew").addEventListener("click", init);
 document.getElementById("btnRoll").addEventListener("click", function roll() {
   // if it's the current player's turn
   // Getting random numbers for the dice :
-  var die1Random = Math.floor(Math.random() * 6 + 1);
-  var die2Random = Math.floor(Math.random() * 6 + 1);
+  die1Random = random();
+  die2Random = random();
 
   var die1 = document.getElementById("die1");
   var die2 = document.getElementById("die2");
@@ -95,7 +101,7 @@ document.getElementById("btnRoll").addEventListener("click", function roll() {
 });
 
 function doubleSkunk() {
-  score[activePlayer] = "0";
+  score[activePlayer] = 0;
   console.log(score[activePlayer]);
   document.querySelector("#score" + (activePlayer + 1)).textContent =
     score[activePlayer];
@@ -124,46 +130,87 @@ function nextPlayer() {
 // Click on HOLD BUTTON
 document.getElementById("btnHold").addEventListener("click", function roll() {
   // if it's the current player's turn
-  score[activePlayer] += currentValue; // adding the current score to the player's global score
+  if (die1Random !== 1 && die2Random !== 1 && die1Random !== die2Random) {
+    score[activePlayer] += currentValue;
+    // updating the player's current score
+    document.querySelector("#score" + (activePlayer + 1)).textContent =
+      score[activePlayer];
 
-  // updating the player's current score
-  document.querySelector("#score" + (activePlayer + 1)).textContent =
-    score[activePlayer];
+    // getting and setting the winning score
+    var input = document.querySelector(".winningScore").value;
+    var winningScore;
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
 
-  // getting and setting the winning score
-  var input = document.querySelector(".winningScore").value;
-  var winningScore;
-  if (input) {
-    winningScore = input;
-  } else {
-    winningScore = 100;
+    // when there's a winner
+    if (score[activePlayer] >= winningScore) {
+      // overlay
+      const win = document.getElementById("overlay");
+      win.style.display = "block";
+      setTimeout(() => (win.style.display = "none"), 5000);
+
+      document
+        .querySelector(".player" + (activePlayer + 1) + "Zone")
+        .classList.add("winner");
+      document
+        .querySelector(".player" + (activePlayer + 1) + "Zone")
+        .classList.remove("active");
+    } else console.log("on va au joueur suivant");
+    nextPlayer();
+  } // adding the current score to the player's global score
+  else if (die1Random !== 1 && die2Random !== 1 && die1Random === die2Random) {
+    score[activePlayer] += currentValue;
+    document.querySelector("#score" + (activePlayer + 1)).textContent =
+      score[activePlayer];
+
+    if (activePlayer === 0) {
+      score[1] -= currentValue;
+      if (score[1] < 0) {
+        score[1] = 0;
+      } else score[1];
+      document.querySelector("#score2").textContent = score[1];
+    } else {
+      score[0] -= currentValue;
+      if (score[0] < 0) {
+        score[0] = 0;
+      } else score[0];
+      document.querySelector("#score1").textContent = score[0];
+    }
+    // getting and setting the winning score
+    var input = document.querySelector(".winningScore").value;
+    var winningScore;
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    // when there's a winner
+    if (score[activePlayer] >= winningScore) {
+      // overlay
+      const win = document.getElementById("overlay");
+      win.style.display = "block";
+      setTimeout(() => (win.style.display = "none"), 5000);
+
+      document
+        .querySelector(".player" + (activePlayer + 1) + "Zone")
+        .classList.add("winner");
+      document
+        .querySelector(".player" + (activePlayer + 1) + "Zone")
+        .classList.remove("active");
+    } else console.log("on va au joueur suivant");
+    nextPlayer();
   }
-
-  // when there's a winner
-  if (score[activePlayer] >= winningScore) {
-    // overlay
-    const win = document.getElementById("overlay");
-    win.style.display = "block";
-    setTimeout(() => (win.style.display = "none"), 5000);
-
-    document
-      .querySelector(".player" + (activePlayer + 1) + "Zone")
-      .classList.add("winner");
-    document
-      .querySelector(".player" + (activePlayer + 1) + "Zone")
-      .classList.remove("active");
-  } else nextPlayer();
 });
 
 // échelle en bas montrant l'évolution des points de chaque joueur (comme une course) avec ligne d'arrivée
 // lorsqu'il y a un double, si le joueur décide de garder la somme, elle lui est augmentée (comme d'habitude),mais elle est aussi retirée à l'autre joueur
-// lorsqu'un "1" apparaît sur un dé, ça devient directemnt le tour de l'autre joueur
-// page de garde avec le nom du jeu, et 2 inputs pour insérer le nom des joueurs (et reprendre les noms ensuite)
-// ajouter pictogrammes pour les boutons
-// bouton "?" avec règles du jeu
-// changer format/CSS de la page (background image...)
 
-// changer addEventListener à onClick
+// page de garde avec le nom du jeu, et 2 inputs pour insérer le nom des joueurs (et reprendre les noms ensuite)
+// bouton "?" avec règles du jeu
+
 // pb ac New Game button
 // add the name of the winner
-// commence pas au 1er lancé
